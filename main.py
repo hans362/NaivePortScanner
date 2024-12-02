5bc087e8bfedce98a95bc86fd204d7c62ab12b8e
hans362 <i@hans362.cn>
2024-12-02 11:05:02 +0800

    feat: async programming

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from tkinter import messagebox
@@ -5,29 +6,85 @@ from tkinter import messagebox
 import consts
 import utils
 
+total_tasks = 0
+finished_tasks = 0
+
+
+def detect_hosts_callback(result):
+    global total_tasks, finished_tasks
+    finished_tasks += 1
+    ip, status = result
+    if status == consts.HOST_UP:
+        tree.insert("", "end", text=ip)
+    progressbar["value"] = finished_tasks / total_tasks * 100
+    if finished_tasks == total_tasks:
+        detect_button["state"] = "normal"
+        scan_button["state"] = "normal"
+        messagebox.showinfo("探测完成", "探测完成")
+
 
 def detect_hosts():
+    global total_tasks, finished_tasks
     cidr = cidr_entry.get()
-    ports = ports_entry.get()
-    if not cidr or not ports:
-        messagebox.showwarning("错误", "请输入有效的 CIDR 和端口范围")
+    if not cidr:
+        messagebox.showwarning("错误", "请输入有效的 CIDR")
         return
     ips = utils.CidrToIps(cidr)
     if len(ips) == 0:
         messagebox.showwarning("错误", "请输入有效的 CIDR")
         return
+    detect_button["state"] = "disabled"
+    scan_button["state"] = "disabled"
+    total_tasks = len(ips)
+    finished_tasks = 0
     tree.delete(*tree.get_children())
-    results = utils.ParallelIcmpEchoScan(ips)
-    for i, result in enumerate(results):
-        if result == consts.HOST_UP:
-            tree.insert("", "end", text=ips[i])
-    messagebox.showinfo("扫描完成", "扫描完成")
+    utils.ParallelIcmpEchoScan(ips, detect_hosts_callback)
+
+
+def scan_callback(result):
+    global total_tasks, finished_tasks
+    finished_tasks += 1
+    ip, port, status = result
+    for record in tree.get_children():
+        if tree.item(record)["text"] == ip:
+            break
+    else:
+        record = tree.insert("", "end", text=ip)
+    for item in tree.get_children(record):
+        if tree.item(item)["text"] == "Open":
+            open = item
+        elif tree.item(item)["text"] == "Filtered":
+            filtered = item
+        elif tree.item(item)["text"] == "Closed":
+            closed = item
+        elif tree.item(item)["text"] == "Open/Filtered":
+            open_or_filtered = item
+        elif tree.item(item)["text"] == "Closed/Filtered":
+            closed_or_filtered = item
+    if status == consts.PORT_OPEN:
+        tree.insert(open, "end", text=f"{port}/TCP")
+        tree.item(open, open=True)
+        tree.item(record, open=True)
+    elif status == consts.PORT_FILTERED:
+        tree.insert(filtered, "end", text=f"{port}/TCP")
+    elif status == consts.PORT_CLOSED:
+        tree.insert(closed, "end", text=f"{port}/TCP")
+    elif status == consts.PORT_OPEN | consts.PORT_FILTERED:
+        tree.insert(open_or_filtered, "end", text=f"{port}/TCP")
+    elif status == consts.PORT_CLOSED | consts.PORT_FILTERED:
+        tree.insert(closed_or_filtered, "end", text=f"{port}/TCP")
+    progressbar["value"] = finished_tasks / total_tasks * 100
+    if finished_tasks == total_tasks:
+        detect_button["state"] = "normal"
+        scan_button["state"] = "normal"
+        messagebox.showinfo("扫描完成", "扫描完成")
 
 
 def scan_ports():
-    ips = [item for item in tree.get_children()]
+    global total_tasks, finished_tasks
+    ips = [tree.item(record)["text"] for record in tree.get_children()]
     if len(ips) == 0:
-        messagebox.showwarning("错误", "请先探测存活主机")
+        messagebox.showwarning("错误", "请先进行主机探测")
         return
     ports = []
     for port in ports_entry.get().split(","):
@@ -49,30 +106,19 @@ def scan_ports():
     else:
         messagebox.showwarning("错误", "请选择有效的扫描方法")
         return
-    for record in ips:
-        ip = tree.item(record)["text"]
-        open_ports = utils.ParallelPortScan(ip, ports, scan_function)
+    detect_button["state"] = "disabled"
+    scan_button["state"] = "disabled"
+    for record in tree.get_children():
         for item in tree.get_children(record):
             tree.delete(item)
-        tree.item(record, open=True)
-        open = tree.insert(record, "end", text="Open")
-        filtered = tree.insert(record, "end", text="Filtered")
-        closed = tree.insert(record, "end", text="Closed")
-        open_or_filtered = tree.insert(record, "end", text="Open/Filtered")
-        closed_or_filtered = tree.insert(record, "end", text="Closed/Filtered")
-        for port, status in zip(ports, open_ports):
-            if status == consts.PORT_OPEN:
-                tree.insert(open, "end", text=f"{port}/TCP")
-                tree.item(open, open=True)
-            elif status == consts.PORT_FILTERED:
-                tree.insert(filtered, "end", text=f"{port}/TCP")
-            elif status == consts.PORT_CLOSED:
-                tree.insert(closed, "end", text=f"{port}/TCP")
-            elif status == consts.PORT_OPEN | consts.PORT_FILTERED:
-                tree.insert(open_or_filtered, "end", text=f"{port}/TCP")
-            elif status == consts.PORT_CLOSED | consts.PORT_FILTERED:
-                tree.insert(closed_or_filtered, "end", text=f"{port}/TCP")
-    messagebox.showinfo("扫描完成", "扫描完成")
+        tree.insert(record, "end", text="Open")
+        tree.insert(record, "end", text="Filtered")
+        tree.insert(record, "end", text="Closed")
+        tree.insert(record, "end", text="Open/Filtered")
+        tree.insert(record, "end", text="Closed/Filtered")
+    total_tasks = len(ips) * len(ports)
+    finished_tasks = 0
+    utils.ParallelPortScan(ips, ports, scan_callback, scan_function)
 
 
 root = ttk.Window(themename="superhero")
@@ -88,21 +134,40 @@ ports_entry = ttk.Entry(root)
 ports_entry.grid(row=1, column=1, padx=10, pady=10, sticky="ew")
 ports_entry.insert(0, consts.DEFAULT_PORT_RANGE)
 
-detect_button = ttk.Button(root, text="探测存活主机", command=detect_hosts)
+detect_button = ttk.Button(root, text="主机探测", command=detect_hosts)
 detect_button.grid(row=0, column=2, padx=10, pady=10, sticky="ew")
 
-scan_button = ttk.Button(root, text="开始端口扫描", command=scan_ports)
+scan_button = ttk.Button(root, text="端口扫描", command=scan_ports)
 scan_button.grid(row=1, column=2, padx=10, pady=10, sticky="ew")
 
 
 tree = ttk.Treeview(root)
 tree.heading("#0", text="扫描结果")
 tree.grid(row=2, column=0, columnspan=3, padx=10, pady=10, sticky="nsew")
+vsb = ttk.Scrollbar(tree, orient="vertical", command=tree.yview)
+vsb.pack(side="right", fill="y")
+tree.configure(yscrollcommand=vsb.set)
 
-tcp_scan_method = ttk.Combobox(root)
+settings = ttk.Frame(root)
+settings.grid(row=3, column=0, columnspan=3, padx=0, pady=0, sticky="ew")
+
+detect_method = ttk.Combobox(settings)
+detect_method["values"] = ("ICMP Echo 探测",)
+detect_method["state"] = "readonly"
+detect_method.current(0)
+detect_method.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
+
+tcp_scan_method = ttk.Combobox(settings)
 tcp_scan_method["values"] = ("TCP Connect 扫描", "TCP SYN 扫描", "TCP FIN 扫描")
+tcp_scan_method["state"] = "readonly"
 tcp_scan_method.current(0)
-tcp_scan_method.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
+tcp_scan_method.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
+
+export_button = ttk.Button(settings, text="导出结果")
+export_button.grid(row=0, column=2, padx=10, pady=10, sticky="ew")
+
+settings.grid_columnconfigure(0, weight=1)
+settings.grid_columnconfigure(1, weight=1)
 
 
 progressbar = ttk.Progressbar(root, mode="determinate")
--- utils.py
+++ utils.py
@@ -19,16 +19,15 @@ def CidrToIps(cidr):
 def IcmpEchoScan(ip):
     response = sr1(IP(dst=ip) / ICMP(), timeout=consts.SCAN_TIMEOUT, verbose=False)
     if response:
-        return consts.HOST_UP
-    return consts.HOST_DOWN
+        return ip, consts.HOST_UP
+    return ip, consts.HOST_DOWN
 
 
-def ParallelIcmpEchoScan(ips):
+def ParallelIcmpEchoScan(ips, callback):
     pool = ThreadPool(os.cpu_count() * 4 + 1)
-    results = pool.map(IcmpEchoScan, ips)
+    for ip in ips:
+        pool.apply_async(IcmpEchoScan, args=(ip,), callback=callback)
     pool.close()
-    pool.join()
-    return results
 
 
 def TcpConnectScan(ip, port):
@@ -37,8 +36,8 @@ def TcpConnectScan(ip, port):
     response = s.connect_ex((ip, port))
     s.close()
     if response == 0:
-        return consts.PORT_OPEN
-    return consts.PORT_CLOSED | consts.PORT_FILTERED
+        return ip, port, consts.PORT_OPEN
+    return ip, port, consts.PORT_CLOSED | consts.PORT_FILTERED
 
 
 def TcpSynScan(ip, port):
@@ -52,10 +51,10 @@ def TcpSynScan(ip, port):
         if response.haslayer(TCP):
             flags = response.getlayer(TCP).flags
             if "S" in flags and "A" in flags:
-                return consts.PORT_OPEN
+                return ip, port, consts.PORT_OPEN
             elif "R" in flags:
-                return consts.PORT_CLOSED
-    return consts.PORT_FILTERED
+                return ip, port, consts.PORT_CLOSED
+    return ip, port, consts.PORT_FILTERED
 
 
 def TcpFinScan(ip, port):
@@ -69,16 +68,16 @@ def TcpFinScan(ip, port):
         if response.haslayer(TCP):
             flags = response.getlayer(TCP).flags
             if "R" in flags:
-                return consts.PORT_CLOSED
-    return consts.PORT_OPEN | consts.PORT_FILTERED
+                return ip, port, consts.PORT_CLOSED
+    return ip, port, consts.PORT_OPEN | consts.PORT_FILTERED
 
 
-def ParallelPortScan(ip, ports, scan_type=TcpConnectScan):
+def ParallelPortScan(ips, ports, callback, scan_type=TcpConnectScan):
     pool = ThreadPool(os.cpu_count() * 4 + 1)
-    results = pool.starmap(scan_type, [(ip, port) for port in ports])
+    for ip in ips:
+        for port in ports:
+            pool.apply_async(scan_type, args=(ip, port), callback=callback)
     pool.close()
-    pool.join()
-    return results
 
 
 if __name__ == "__main__":
